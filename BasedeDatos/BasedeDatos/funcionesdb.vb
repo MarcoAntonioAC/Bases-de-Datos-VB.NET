b2f4bdf4dcf8d490139ecfb82ea6d7b90c4a64ee
MarcoAntonioAC <marco_antonio1997@hotmail.com>
2013-07-05 23:42:45 -0500

    Creación de Base de Datos
    Primera conexión y prueba con Access desde Visual Basic.NET

---
+++ BasedeDatos/BasedeDatos/funcionesdb.vb
@@ -0,0 +1,14 @@
+﻿Module funcionesdb
+
+    Public conn As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=BasedeDatos1.accdb;Persist Security Info=False")
+
+    Public Sub conexion()
+        Try
+            conn.Open()
+            MsgBox("¡Conexion Exitosa!")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+End Module
